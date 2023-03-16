036fc0c72145c83277a39f23d83f3dd8db8dc7bd
Emma Wilkinson <emmawilkinsonnxo@gmail.com>
2023-03-16 21:20:52 +0000

    added ship object, and constructor

---
+++ __tests__/ship.test.js
@@ -0,0 +1,15 @@
+const Ship = require('../src/ship.js');
+const ship = new Ship('Leeds');
+
+describe('Ship', () => {
+    test('creating the ship', () => { 
+
+    expect(new Ship()).toBeInstanceOf(Object);
+});
+    
+    test('has a starting port', () => {
+        const ship = new Ship('Leeds');
+
+        expect(ship.startingPort).toEqual('Leeds');
+    });
+});
